ee73de05f7d15528b760010682a2459237e543a6
Kenneth Kutschera <kennethchoi19@gmail.com>
2017-04-22 09:32:51 -0400

    Created the header part of our course. Continuing the rest of the course soon.

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -7,10 +7,19 @@ import React from 'react';
 
 class Header extends React.Component {
 	render() {
+		console.log(this);
 		return (
 			<header className="top">
-			<h1>Catch of the day</h1>
-			<h3 className="tagline">{this.props.tagline}</h3>
+			{/*this.props.tagline is a variable that you just inserted from the Header tagline*/}
+			<h1>Catch
+			<span className="ofThe">
+				<span className="of">of 
+				</span>
+				<span className="the">the
+				</span>
+			</span>
+			day</h1>
+			<h3 className="tagline"><span>{this.props.tagline}</span></h3>
 			</header>
 		)
 	}
